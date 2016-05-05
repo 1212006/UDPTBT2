18652415a364ac1abff8deb351722d862e257d01
libravn <1212437@student.hcmus.edu.vn>
2016-05-05 19:46:34 +0700

    - Cap nhat Signup - Sua doi homepage - Hoan tat su dung csdl local

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -2,10 +2,52 @@
  * UserController
  *
  * @description :: Server-side logic for managing users
- * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 
 module.exports = {
-	
-};
+  /**
+   * Sign up for a user account.
+   */
+   signup: function(req,res){
 
+	   var Passwords = require('machinepack-passwords');
+
+		// Encrypt a string using the BCrypt algorithm.
+		Passwords.encryptPassword({
+		password: req.param('password'),
+		}).exec({
+		// An unexpected error occurred.
+		error: function (err){
+			return res.negotiate(err);
+		},
+		// OK.
+		success: function (encryptedPassword){
+	   		
+			//Create a User with the param sent from the signup form
+			User.create({
+				name: req.param('name'),
+				email: req.param('email'),
+				encryptedPassword: encryptedPassword
+			}, function userCreated(err, newUser){
+				if(err){
+					console.log("err: ",err);
+					console.log("err.invalidAttributes: ", err.invalidAttributes);
+
+					//mail da su dung
+	              	if (err.invalidAttributes && err.invalidAttributes.email && err.invalidAttributes.email[0]
+	                && err.invalidAttributes.email[0].rule === 'unique') {
+	                return res.emailAddressInUse();
+					}
+					//
+					return res.negotiate(err);
+				}
+				return res.json({
+					id: newUser.id
+				});
+   			
+	   		});
+		}
+	});
+	}
+};
